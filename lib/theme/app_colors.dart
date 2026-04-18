54c575927f08bd1b4c2087b118bc0a71cdd266d1
Ilxom <ilhomjonisoqjonov2@gmail.com>
2026-04-18 17:05:18 +0500

    feat: colors

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -7,6 +7,10 @@ class AppColors {
   static const Color brandGreenDeep = Color(0xFF011606);
   static const Color brandGreen = Color(0xFF0A6B23);
   static const Color splashGreen = Color(0xFF00E135);
+  static const Color greenBlack = Color(0xFF000702);
+
+  static const Color textBlack = Color(0xFF18181B);
+  static const Color buttonTextBlack = Color(0xFF151515);
 
   static const Color surface = Color(0xFFF5F7F4);
   static const Color onSurface = Color(0xFF0E1A12);
